f737a3397df27acfeb5aebb05b204bddb12b6278
Robert Blade <mail@robertsandiford.co.uk>
2022-04-15 02:41:37 +0100

    great

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,19 +5,8 @@ export default {
     mount: {
         public: '/',
         ReactApp: '/dist',
-        config: '/config'
-    },
-    plugins: [
-    ],
-    routes: [
-    ],
-    optimize: {
     },
     packageOptions: {
         //polyfillNode: true
-    },
-    devOptions: {
-    },
-    buildOptions: {
     },
 }

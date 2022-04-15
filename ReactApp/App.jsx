b3df5f90074b9d4e57e8b4141dfccf5231a53627
Robert Blade <mail@robertsandiford.co.uk>
2022-04-15 02:24:16 +0100

    close

--- ReactApp/App.jsx
+++ ReactApp/App.jsx
@@ -2,6 +2,9 @@
 import React from 'react'
 
 import { RenderRoutes } from './Modules/Routing/RenderRoutes.jsx'
+//import { AppComponent } from './AppComponent/AppComponent.jsx'
+import { SxyComponent } from './Modules/Sxy/SxyComponent.jsx'
+import { goto } from './Modules/Navigation/goto.js'
 
 export function App() {
     return (
--- ReactApp/Modules/Routing/RenderRoutes.jsx
+++ ReactApp/Modules/Routing/RenderRoutes.jsx
@@ -3,6 +3,5 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom' // eslint-disable-line no-unused-vars
 
-import { AppComponent } from '../../AppComponent/AppComponent.jsx'
 
 export const RenderRoutes = function() {}

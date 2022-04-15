8b6fb4fcd86552ed7a845f8834524051a1aa4f5c
Robert Blade <mail@robertsandiford.co.uk>
2022-04-15 02:25:50 +0100

    try deleting some more

--- ReactApp/App.jsx
+++ ReactApp/App.jsx
@@ -1,10 +1,12 @@
 
 import React from 'react'
 
-import { RenderRoutes } from './Modules/Routing/RenderRoutes.jsx'
-//import { AppComponent } from './AppComponent/AppComponent.jsx'
-import { SxyComponent } from './Modules/Sxy/SxyComponent.jsx'
-import { goto } from './Modules/Navigation/goto.js'
+// import { RenderRoutes } from './Modules/Routing/RenderRoutes.jsx'
+// import { goto } from './Modules/Navigation/goto.js'
+
+
+import { withRouter } from 'react-router-dom' // eslint-disable-line no-unused-vars
+import { matchPath } from 'react-router'
 
 export function App() {
     return (
